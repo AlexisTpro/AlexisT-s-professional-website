9641b00de996b4b4b14a1b085123fc20adae0671
AlexisTpro <139190477+AlexisTpro@users.noreply.github.com>
2023-07-11 04:06:19 +0200

    Add files via upload

---
+++ things.js
@@ -0,0 +1,9 @@
+let togg1 = document.getElementById("box1");
+
+function togg1.addEventListener("click", () => {
+  if(getComputedStyle(n1).display != "none"){
+    n1.style.display = "none";
+  } else {
+    n1.style.display = "n1";
+  }
+})
